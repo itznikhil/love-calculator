2b01bb199a9d1f8c2f5cad8db33021d991bb180e
itznikhil <79260433+itznikhil@users.noreply.github.com>
2021-05-23 11:02:58 +0530

    Initial commit

---
+++ assets/Assets.js
@@ -0,0 +1,8 @@
+const Assets = {
+    logo: require('./logo.png'),
+    loveAnimation: require('./resultanim.json'),
+    manIcon: require('./man.png'),
+    womanIcon: require('./woman.png'),
+}
+
+export default Assets;
